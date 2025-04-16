74be10895681eb8d3d148ee028ea03c9c7130995
jambuttenshaw <jamie.buttenshaw@gmail.com>
2025-04-16 11:44:05 +0100

    Implemented sobel filter render pass

--- Source/NPRTools/NPRTools.Build.cs
+++ Source/NPRTools/NPRTools.Build.cs
@@ -39,6 +39,8 @@ public class NPRTools : ModuleRules
 				"Slate",
 				"SlateCore",
 				"RenderCore",
+				"RHI",
+				"Renderer"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
--- Source/NPRTools/Private/NPRToolsViewExtension.cpp
+++ Source/NPRTools/Private/NPRToolsViewExtension.cpp
@@ -1,14 +1,87 @@
 #include "NPRToolsViewExtension.h"
 
+#include "ShaderParameterStruct.h"
+#include "ScreenPass.h"
+
+#include "PostProcess/PostProcessInputs.h"
+#include "SceneTextures.h"
+#include "FXRenderingUtils.h"
+
+
+class FSobelPassPS : public FGlobalShader
+{
+public:
+	DECLARE_GLOBAL_SHADER(FSobelPassPS);
+	SHADER_USE_PARAMETER_STRUCT(FSobelPassPS, FGlobalShader);
+
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER_STRUCT_REF(FViewUniformShaderParameters, View)
+
+		SHADER_PARAMETER_STRUCT(FScreenPassTextureViewportParameters, ViewPort)
+
+		SHADER_PARAMETER_RDG_TEXTURE_SRV(Texture2D<float4>, SceneColor)
+
+		RENDER_TARGET_BINDING_SLOTS()
+	END_SHADER_PARAMETER_STRUCT()
+};
+
+IMPLEMENT_GLOBAL_SHADER(FSobelPassPS, "/NPRTools/Sobel.usf", "SobelPassPS", SF_Pixel);
+
 
 FNPRToolsViewExtension::FNPRToolsViewExtension(const FAutoRegister& AutoRegister)
 	: FSceneViewExtensionBase(AutoRegister)
 {
-	UE_LOG(LogTemp, Display, TEXT("Created NPR tools view extension."));
 }
 
 void FNPRToolsViewExtension::PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& View, const FPostProcessingInputs& Inputs)
 {
-	// Perform rendering work
-	UE_LOG(LogTemp, Display, TEXT("Performing pre-post-process pass NPR rendering work."));
+	// Get the scene colour texture from the post process inputs
+	Inputs.Validate();
+	FRDGTextureRef SceneColor = (*Inputs.SceneTextures)->SceneColorTexture;
+
+	// Create a texture to hold the output of our Sobel filter
+	// It should be the same format etc as the scene colour texture
+	FRDGTextureDesc SobelOutputDesc = SceneColor->Desc;
+	SobelOutputDesc.ClearValue = FClearValueBinding(FLinearColor(0.0f, 0.0f, 0.0f));
+
+	FRDGTextureRef SobelOutputTexture = GraphBuilder.CreateTexture(SobelOutputDesc, TEXT("SobelOutput"));
+
+	// We want to perform our postprocessing to the entire viewport
+	FScreenPassTextureViewport ViewPort(UE::FXRenderingUtils::GetRawViewRectUnsafe(View));
+
+	// Perform sobel filter pass
+	{
+		// Set up shader parameters
+		FSobelPassPS::FParameters* PassParameters = GraphBuilder.AllocParameters<FSobelPassPS::FParameters>();
+
+		PassParameters->View = View.ViewUniformBuffer;
+		PassParameters->ViewPort = GetScreenPassTextureViewportParameters(ViewPort);
+
+		PassParameters->SceneColor = GraphBuilder.CreateSRV(SceneColor);
+		PassParameters->RenderTargets[0] = FRenderTargetBinding(SobelOutputTexture, ERenderTargetLoadAction::EClear);
+
+		// Retrieve our pixel shader from the global shader map
+		const FGlobalShaderMap* ShaderMap = GetGlobalShaderMap(View.GetFeatureLevel());
+		TShaderMapRef<FSobelPassPS> PixelShader(ShaderMap);
+
+		// Add a full-screen pass to the render graph
+		AddDrawScreenPass(
+			GraphBuilder,
+			RDG_EVENT_NAME("SobelFilter"),
+			View,
+			ViewPort,
+			ViewPort,
+			PixelShader,
+			PassParameters
+		);
+	}
+
+	// Copy our output back to the back buffer
+	{
+		AddCopyTexturePass(
+			GraphBuilder,
+			SobelOutputTexture,
+			SceneColor
+		);
+	}
 }
